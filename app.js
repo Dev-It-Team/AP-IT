2c8eb0430cb6418f18fab6b82754f54e0bbb65dc
Gabriel Jouet <gabriel.jouet@viacesi.fr>
2021-06-22 11:34:29 +0200

    Added a custom 404 error page

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ app.use('/components', componentsRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) 
 {
-  next(createError(404));
+  res.status(404);
+  res.send({ "error": "Route not found" });
 });
 
 // error handler
