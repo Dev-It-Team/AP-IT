0b276c08bd816866a6ee4a8ff03fc11cf604a2ba
Antonin <antonin.jacob@viacesi.fr>
2021-06-23 11:40:44 +0200

    Moved .sql and .json data to /data folder

--- Delivers.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO delivers VALUES (6, '15 Rue Jean Lille 59000', 'Voiture', 150, 65);
-INSERT INTO delivers VALUES (7, '12 Rue Jean Lille 59000', 'Scooter', 350, 100);
-INSERT INTO delivers VALUES (8, '158 Rue Jean Lille 59000', 'Pieds', 75, 18);
